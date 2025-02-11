2c633e836bf8269000f454bde24b28f8c08c51f9
AndroidWiz <rashedc004@gmail.com>
2025-02-11 06:14:25 +0600

    markdown removed

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -1,10 +1,11 @@
 import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+import kotlinx.html.link
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kobweb.application)
-    alias(libs.plugins.kobwebx.markdown)
+//    alias(libs.plugins.kobwebx.markdown)
 }
 
 group = "com.demo.riverstonehomesmanagement"
@@ -13,7 +14,16 @@ version = "1.0-SNAPSHOT"
 kobweb {
     app {
         index {
-            description.set("Powered by Kobweb")
+            description.set("Riverstone Homes Management – We Manage Your Home")
+
+            head.add {
+                link(rel = "preconnect", href = "https://fonts.googleapis.com")
+                link(rel = "preconnect", href = "https://fonts.gstatic.com") { attributes["crossorigin"] = "" }
+                link(
+                    href = "https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap",
+                    rel = "stylesheet"
+                )
+            }
         }
     }
 }
@@ -35,8 +45,8 @@ kotlin {
             implementation(libs.kobweb.silk)
             // This default template uses built-in SVG icons, but what's available is limited.
             // Uncomment the following if you want access to a large set of font-awesome icons:
-            // implementation(libs.silk.icons.fa)
-            implementation(libs.kobwebx.markdown)
+             implementation(libs.silk.icons.fa)
+//            implementation(libs.kobwebx.markdown)
         }
 
         // Uncomment the following if you pass `includeServer = true` into the `configAsKobwebApplication` call.
